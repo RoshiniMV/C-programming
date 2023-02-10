be1b35a70c78f7d6663fa303bf4048d4324d2669
RoshiniMV <120188146+RoshiniMV@users.noreply.github.com>
2023-02-10 19:16:09 +0530

    done

---
+++ sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+char alpha;
+printf("enter the numbers:");
+scanf("%c",&alpha);
+switch (alpha)
+{
+case 'a': printf("is a vowel");
+         break;
+case 'e': printf("is a vowel");
+         break;
+case 'i':printf("is a vowel");
+         break;
+case 'o':printf("is a vowel");
+         break;
+case 'u': printf("is a vowel");
+         break;
+default: printf("consonants");
+         break;
+         }
+         }
